0357de699d082b371aa2cdb9eb904b72bf64ef51
kady boy <kdiboy1216@gmail.com>
2016-03-30 21:42:23 +0900

    Finish layout and routes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
+	
   def home
   end
 
@@ -8,4 +9,7 @@ class StaticPagesController < ApplicationController
   def about
   end
   
+  def contact
+  end
+
 end
